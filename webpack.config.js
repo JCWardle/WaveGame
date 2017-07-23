5e0905a59cb4a8a37339afc5caa21d0ea8e9fa9c
Jordan Wardle <jordan@ninjatertle.com>
2017-07-23 15:57:52 +1000

    Got typescript debugging working with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 var path = require('path');
+var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: "inline-source-map",
-  devServer: {
-        contentBase: "./"
-    }
+  devtool: "source-map",
+  resolve: {
+    extensions: ['.ts', '.tsx','.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ]
 };
