3d46b855d6f7e2a354c6e41bfc6c924430349f73
Jordan Wardle <jordan@ninjatertle.com>
2017-07-23 20:05:43 +1000

    Refereneced the bundle correctly for the dev server.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 class Startup {
     public static main(): number {
-        console.log('Hello World');
-        alert('Hello World');
+        let canvas:HTMLCanvasElement = document.createElement('canvas');
+        canvas.id = 'GameCanvas';
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        document.body.appendChild(canvas);
+
+        let context:CanvasRenderingContext2D = canvas.getContext('2d');
+        context.strokeRect(0,0, 500, 500);
         return 0;
     }
 }
-
+console.log('Nice!');
 Startup.main();
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,17 @@ var webpack = require("webpack");
 module.exports = {
   entry: './src/index.ts',
   output: {
-    filename: 'bundle.js',
+    filename: './dist/bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: "source-map",
+  devtool: "inline-source-map",
   resolve: {
     extensions: ['.ts', '.tsx','.js']
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts/,
         loader: 'ts-loader',
         exclude: /node_modules/
       }
